08ae22a8905447aab8ab8dc3d40ba88b85a609ec
Shreevardhan <vardhansasuke@gmail.com>
2026-05-02 16:03:40 +0530

    rebasing the whole thing with main

--- scripts/sh-files/build.sh
+++ scripts/sh-files/build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#cd ../../cloud-config-servier
-#gradle build -x test
-#echo "completed building cloud-config-servier"
+cd ../../cloud-config-servier
+gradle clean build -x test
+echo "completed building cloud-config-servier"
 
 #cd ../eureka-server
 #gradle build -x test
 #echo "completed building eureka-server"
 
-cd ../../gateway
-gradle build -x test
+cd ../gateway
+gradle clean build -x test
 echo "completed building gateway"
 
 cd ../order
-gradle build -x test
+gradle clean build -x test
 echo "completed building order"
 
 cd ../payment
-gradle build -x test
+gradle clean build -x test
 echo "completed building payment"
 
 #cd ../spring-security-jwt
