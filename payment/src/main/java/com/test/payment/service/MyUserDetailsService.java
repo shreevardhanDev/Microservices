aeef48708db5382f202bbce4e333b77a211a341e
Shreevardhan <vardhansasuke@gmail.com>
2026-05-09 18:18:05 +0530

    fixed cleanup. of kafka, spirng security etc.

--- payment/src/main/java/com/test/payment/service/MyUserDetailsService.java
+++ payment/src/main/java/com/test/payment/service/MyUserDetailsService.java
@@ -1,38 +1,38 @@
-package com.test.payment.service;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.test.payment.entities.Users;
-import com.test.payment.repository.UserDao;
-
-@Service
-public class MyUserDetailsService implements UserDetailsService {
-
-	@Autowired
-	private UserDao userDao;
-
-	@Override
-	@Transactional
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-		User userDet = null;
-		try {
-			Users user = userDao.getUserForEmployeeHrmsId(userName);
-			userDet = new User(user.getEmployeeHrmsId(), user.getPassword(),
-					Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
-		} catch (Exception ex) {
-			throw new UsernameNotFoundException("Not found: " + userName);
-		}
-
-		return userDet;
-	}
-
-}
+//package com.test.payment.service;
+//
+//import java.util.Arrays;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.test.payment.entities.Users;
+//import com.test.payment.repository.UserDao;
+//
+//@Service
+//public class MyUserDetailsService implements UserDetailsService {
+//
+//	@Autowired
+//	private UserDao userDao;
+//
+//	@Override
+//	@Transactional
+//	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//		User userDet = null;
+//		try {
+//			Users user = userDao.getUserForEmployeeHrmsId(userName);
+//			userDet = new User(user.getEmployeeHrmsId(), user.getPassword(),
+//					Arrays.asList(new SimpleGrantedAuthority(user.getRole())));
+//		} catch (Exception ex) {
+//			throw new UsernameNotFoundException("Not found: " + userName);
+//		}
+//
+//		return userDet;
+//	}
+//
+//}
